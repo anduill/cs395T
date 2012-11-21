42d571a9f681e58d08a31f04bd1d5cf918d8e7a4
David Garcia <anduill@yahoo.com>
2012-11-20 18:19:03 -0600

    finished histogram buidling function

--- project/GCmex2.0/getColorHistogramFromCoordinates.m
+++ project/GCmex2.0/getColorHistogramFromCoordinates.m
@@ -10,5 +10,9 @@ function [colorHistogram clusterPoints] = getColorHistogramFromCoordinates(numBi
         colorPoints = [colorPoints; patchPixels];
     end
     [colorPointAssignments clusterPoints] = kmeans(double(colorPoints),numBins);
-    
+    colorHistogram = ones(1,numBins);
+    for assignmentIndex=1:numel(colorPointAssignments)
+        assignment = colorPointAssignments(assignmentIndex);
+        colorHistogram(assignment) = colorHistogram(assignment)+1;
+    end
 end
